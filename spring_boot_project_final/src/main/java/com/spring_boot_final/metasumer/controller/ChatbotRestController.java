e98878eb575c4ed75f0b0953c1587a0f70d1a7eb
Jungi Lee <balll1861@naver.com>
2024-09-13 15:15:41 +0900

    빌드 오류 수정 : mapper, controller, webconfig

--- spring_boot_project_final/src/main/java/com/spring_boot_final/metasumer/controller/ChatbotRestController.java
+++ spring_boot_project_final/src/main/java/com/spring_boot_final/metasumer/controller/ChatbotRestController.java
@@ -26,8 +26,8 @@ public class ChatbotRestController {
 		String result = "";
 		
 		try {
-			//String uploadPath = "D:/springWorkspace/upload/";
-			String uploadPath = "/usr/local/project/upload/"; 
+			String uploadPath = "D:/springWorkspace/upload/";
+			//String uploadPath = "/usr/local/project/upload/"; 
 			String originalFileName = file.getOriginalFilename();
 			String filePathName = uploadPath + originalFileName;
 			
