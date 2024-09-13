5bceb6389d91f4607b42e471586fdfe3c57a3d61
Jungi Lee <balll1861@naver.com>
2024-09-13 17:35:16 +0900

    Update WebConfig.java

--- spring_boot_project_final/src/main/java/com/spring_boot_final/metasumer/WebConfig.java
+++ spring_boot_project_final/src/main/java/com/spring_boot_final/metasumer/WebConfig.java
@@ -1,5 +1,6 @@
 package com.spring_boot_final.metasumer;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,12 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// 프로젝트 외부 경로 이미지 맵핑 설정
+		// 프로젝트 외부 경로 이미지 맵핑 설정 
 		// 맵핑 이름 : project_images
 		registry.addResourceHandler("/project_images/**")
-				.addResourceLocations("file:///D:/springWorkspace/metasumer_images/");
+		.addResourceLocations("file:///usr/local/project/product_images");
+		//.addResourceLocations("file:///D:/springWorkspace/metasumer_images/");
 		registry.addResourceHandler("/ocr_image/**")
-				.addResourceLocations("file:///D:/springWorkspace/metasumer_images/upload/");
-
+		.addResourceLocations("file:///usr/local/project/product_images/upload");
+		//.addResourceLocations("file:///D:/springWorkspace/metasumer_images/upload/");
+		registry.addResourceHandler("/stt_audio/**")
+		.addResourceLocations("file:///usr/local/project/upload/");
+		//.addResourceLocations("file:///D:/springWorkspace/upload/");
+		registry.addResourceHandler("/metasumer_images/**")
+		//.addResourceLocations("file:///usr/local/project/metasumer_images/");
+		.addResourceLocations("file:///D:/springWorkspace/metasumer_images/");
 	}
 }
